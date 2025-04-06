cbe4b5b3a4fe4bc7c810045f58d3461bc5f13ef9
Mohana Misra <iammohanamisra@gmail.com>
2025-04-06 12:44:56 +0530

    Implemented addition of new part to update existing quote by issuing a PATCH request

--- src/components/PartForm/PartForm.jsx
+++ src/components/PartForm/PartForm.jsx
@@ -1,37 +1,88 @@
-import React, {useState, useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 
 const PartForm = ({quote}) => {
-    const [partName, setPartName] = useState('');
-    const [inputVal, setInputVal] = useState('');
     const [allQuantities, setAllQuantities] = useState([]);
+    const [quoteVersion, setQuoteVersion] = useState(0);
+
+    const [inputPartName, setInputPartName] = useState('');
+    const [inputMoq, setInputMoq] = useState('');
+    const [inputQty, setInputQty] = useState('');
+    const [inputPrice, setInputPrice] = useState('');
+
+
 
     const handleAddPrice = (e) => {
         e.preventDefault();
-        setPartName(inputVal);
-        setAllQuantities(Array.from(
-            quote.parts.find(part => part.part_name === partName).prices
-        ));
+        const newQuantityObj = {
+            "qty": Number(inputQty),
+            "unit_price": Number(inputPrice),
+        }
+        console.log(newQuantityObj);
+
+        setAllQuantities([
+            ...allQuantities,
+            newQuantityObj,
+        ])
+
+        setInputQty('');
+        setInputPrice('');
     }
 
-    // useEffect(() => {
-    //
-    // }, [partName])
+    const updateQuote = async (newQuoteObj) => {
+        const url = `http://localhost:3000/quotations/${quote._id}`;
+        await fetch(url, {
+            method: 'PATCH',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(newQuoteObj),
+        });
+        alert("Part Added");
+        const newQuoteVersion = quoteVersion + 1;
+        setQuoteVersion(newQuoteVersion);
+        setInputPartName('');
+        setInputMoq('');
+        setAllQuantities([]);
+    }
+
+    const handleAddPart = async (e) => {
+        e.preventDefault();
+        const newPartObj = {
+            "part_name": inputPartName,
+            "moq": Number(inputMoq),
+            "prices": allQuantities
+        }
+
+        const {_id, currQuote} = quote;
+
+        const updatedQuote = {
+            ...currQuote,
+            parts: [...quote.parts, newPartObj]
+        }
+        await updateQuote(updatedQuote);
+    }
+
+    useEffect(() => {
+        const fetchQuote = async () => {
+            const res = await fetch(`http://localhost:3000/quotations/${quote._id}`);
+            quote = await res.json();
+        };
+        fetchQuote();
+    }, [quoteVersion])
 
     return (
         <form className="space-y-4">
             <div>
                 <label>Part Name</label>
-                <input type="text" value={inputVal} onChange={e => setInputVal(e.target.value)}/>
+                <input type="text" value={inputPartName} onChange={e => setInputPartName(e.target.value)}/>
             </div>
             <div>
                 <label>MOQ</label>
-                <input type="number"/>
+                <input type="number" value={inputMoq} onChange={e => setInputMoq(e.target.value)}/>
             </div>
             <div>
                 <label>Quantity</label>
-                <input type="number"/>
+                <input type="number" value={inputQty} onChange={e => setInputQty(e.target.value)}/>
                 <label>Price</label>
-                <input type="number"/>
+                <input type="number" value={inputPrice} onChange={e => setInputPrice(e.target.value)}/>
                 <button onClick={e => {handleAddPrice(e)}}>Add Price</button>
             </div>
             <table>
@@ -60,7 +111,7 @@ const PartForm = ({quote}) => {
                 }
                 </tbody>
             </table>
-            <button type="submit">Save</button>
+            <button onClick={e=> handleAddPart(e)}>Save</button>
         </form>
     );
 };
