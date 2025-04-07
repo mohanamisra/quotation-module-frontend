eb078deea2ef8dae263c1c89d6a355e17fe5357e
Mohana Misra <iammohanamisra@gmail.com>
2025-04-07 17:57:40 +0530

    Removed index.css, app.css as unnecessary CSS files. Provided comment documentation as and when required.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import './App.css'
 import AllQuotations from "./pages/AllQuotationsScreen/AllQuotations.jsx";
 import AddQuote from "./pages/AddQuoteScreen/AddQuote.jsx";
 import QuotationView from "./pages/QuotationViewScreen/QuotationView.jsx";
+
+// PROVIDING ROUTES TO CERTAIN PAGES
+// NO AUTHORISATION TO ACCESS QUOTES IMPLEMENTED **YET** DUE TO NATURE OF PRODUCT
 
 function App() {
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+
+// STRICT MODE WILL RENDER COMPONENTS TWICE IN DEV MODE ONLY
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
